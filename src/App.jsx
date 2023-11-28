98a885b967bf36c80454c049760bae72a6d0dba7
Abdulrahman Bin Umar <abdulrahmanbinumar178@example.com>
2023-11-28 13:21:35 +0100

    All the files.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx or another root component
+import React from 'react';
+import FootprintTrackerPage from './pages/FootprintTrackerPage';
+
+function App() {
+  return (
+    <div>
+      <h1>Afit Awareness</h1>
+      <FootprintTrackerPage />
+    </div>
+  );
+}
+
+export default App;
